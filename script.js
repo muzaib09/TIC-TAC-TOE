8db443c59b4f8ba135417cb224f483a4589503c4
muzaib09 <muzaibjan02@gmail.com>
2026-06-06 17:38:36 +0500

    Game designed

---
+++ script.js
@@ -0,0 +1,50 @@
+var cell1 = document.getElementById("a1")
+var cell2 = document.getElementById("a2")
+var cell3 = document.getElementById("a3")
+var cell4 = document.getElementById("a4")
+var cell5 = document.getElementById("a5")
+var cell6 = document.getElementById("a6")
+var cell7 = document.getElementById("a7")
+var cell8 = document.getElementById("a8")
+var cell9 = document.getElementById("a9")
+
+var ask = prompt("Enter your value")
+
+if (ask ==="1o") {  cell1.innerHTML = ("O")
+}
+else if (ask ==="2o") {  cell2.innerHTML = ("O")
+}
+else if (ask ==="3o") {  cell3.innerHTML = ("O")
+}
+else if (ask ==="4o") {  cell4.innerHTML = ("O")
+}
+else if (ask ==="5o") {  cell5.innerHTML = ("O")
+}
+else if (ask ==="6o") {  cell6.innerHTML = ("O")
+}
+else if (ask ==="7o") {  cell7.innerHTML = ("O")
+}
+else if (ask ==="8o") {  cell8.innerHTML = ("O")
+}
+else if (ask ==="9o") {  cell9.innerHTML = ("O")
+}
+
+if (ask ==="1x") {  cell1.innerHTML = ("X")
+}
+else if (ask ==="2x") {  cell2.innerHTML = ("X")
+}
+else if (ask ==="3x") {  cell3.innerHTML = ("X")
+}
+else if (ask ==="4x") {  cell4.innerHTML = ("X")
+}
+else if (ask ==="5x") {  cell5.innerHTML = ("X")
+}
+else if (ask ==="6x") {  cell6.innerHTML = ("X")
+}
+else if (ask ==="7x") {  cell7.innerHTML = ("X")
+}
+else if (ask ==="8x") {  cell8.innerHTML = ("X")
+}
+else if (ask ==="9x") {  cell9.innerHTML = ("X")
+}
+
