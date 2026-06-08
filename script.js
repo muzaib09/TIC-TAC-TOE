cc87680095dffd7c25a08a277982044a002f323b
muzaib09 <muzaibjan02@gmail.com>
2026-06-08 06:54:52 -0700

    Functionality added

--- script.js
+++ script.js
@@ -1,50 +1,44 @@
-var cell1 = document.getElementById("a1")
-var cell2 = document.getElementById("a2")
-var cell3 = document.getElementById("a3")
-var cell4 = document.getElementById("a4")
-var cell5 = document.getElementById("a5")
-var cell6 = document.getElementById("a6")
-var cell7 = document.getElementById("a7")
-var cell8 = document.getElementById("a8")
-var cell9 = document.getElementById("a9")
+// variations
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var box3 = document.getElementById("box3");
+var box4 = document.getElementById("box4");
+var box5 = document.getElementById("box5");
+var box6 = document.getElementById("box6");
+var box7 = document.getElementById("box7");
+var box8 = document.getElementById("box8");
+var box9 = document.getElementById("box9");
+var text = "O";
+var turn = 1;
 
-var ask = prompt("Enter your value")
+// functions
+function num(input) {
+  // turn-changer
+  console.log(turn++);
+  if (turn % 2 === 0) {
+    text = "O";
+  } else {
+    text = "X";
+  }
 
-if (ask ==="1o") {  cell1.innerHTML = ("O")
+  // clicktofunction
+  if (input === "box1") {
+    box1.innerHTML = text;
+  } else if (input === "box2") {
+    box2.innerHTML = text;
+  } else if (input === "box3") {
+    box3.innerHTML = text;
+  } else if (input === "box4") {
+    box4.innerHTML = text;
+  } else if (input === "box5") {
+    box5.innerHTML = text;
+  } else if (input === "box6") {
+    box6.innerHTML = text;
+  } else if (input === "box7") {
+    box7.innerHTML = text;
+  } else if (input === "box8") {
+    box8.innerHTML = text;
+  } else if (input === "box9") {
+    box9.innerHTML = text;
+  }
 }
-else if (ask ==="2o") {  cell2.innerHTML = ("O")
-}
-else if (ask ==="3o") {  cell3.innerHTML = ("O")
-}
-else if (ask ==="4o") {  cell4.innerHTML = ("O")
-}
-else if (ask ==="5o") {  cell5.innerHTML = ("O")
-}
-else if (ask ==="6o") {  cell6.innerHTML = ("O")
-}
-else if (ask ==="7o") {  cell7.innerHTML = ("O")
-}
-else if (ask ==="8o") {  cell8.innerHTML = ("O")
-}
-else if (ask ==="9o") {  cell9.innerHTML = ("O")
-}
-
-if (ask ==="1x") {  cell1.innerHTML = ("X")
-}
-else if (ask ==="2x") {  cell2.innerHTML = ("X")
-}
-else if (ask ==="3x") {  cell3.innerHTML = ("X")
-}
-else if (ask ==="4x") {  cell4.innerHTML = ("X")
-}
-else if (ask ==="5x") {  cell5.innerHTML = ("X")
-}
-else if (ask ==="6x") {  cell6.innerHTML = ("X")
-}
-else if (ask ==="7x") {  cell7.innerHTML = ("X")
-}
-else if (ask ==="8x") {  cell8.innerHTML = ("X")
-}
-else if (ask ==="9x") {  cell9.innerHTML = ("X")
-}
-
